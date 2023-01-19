b915a75082be893870b8300acaace9a585c09c44
Gabriel-Uchoa <uchoagabriely@hotmail.com>
2023-01-19 16:31:04 -0300

    estilização adicionada

--- pratica_3.1/src/components/Molecules/CitationCard.jsx
+++ pratica_3.1/src/components/Molecules/CitationCard.jsx
@@ -2,9 +2,14 @@ import Span from "../Atoms/Span"
 import CitationNote from "./CitationNote"
 
 const CitationCard = (props) => {
-
+    const style = {
+        fontSize: "20px",
+        margin: "20px",
+        border: "solid 1px red",
+        padding: "10px"
+    }
     return (
-        <div>
+        <div style={style}>
             <Span>{props.citation}</Span>
             <br />
             <Span>{props.author} - {props.movie} {props.note}</Span>
