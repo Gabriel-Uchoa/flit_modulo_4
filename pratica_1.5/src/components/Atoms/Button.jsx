267a594c4528d36082a46d31937a2f6874d2b651
Gabriel-Uchoa <uchoagabriely@hotmail.com>
2023-01-19 16:11:04 -0300

    correcao rep

--- pratica_1.5/src/components/Atoms/Button.jsx
+++
@@ -1,3 +0,0 @@
-const Button = (props) => <button onClick={props.click} className={props.class} id={props.id}>{props.textContent}</button>
-
-export default Button
--- pratica_1.5/src/components/Atoms/Title.jsx
+++
@@ -1,3 +0,0 @@
-const Title = (props) => <h1 className={props.class} id={props.id}>{props.textContent}</h1>
-
-export default Title
